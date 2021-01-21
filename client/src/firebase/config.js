890b5b59ad87cfb45ceab4d9f19a234b8f9aa584
gsjuradog <gsjuradog@unal.edu.co>
2021-01-21 13:46:33 +0100

    Update config.js

--- client/src/firebase/config.js
+++ client/src/firebase/config.js
@@ -5,13 +5,13 @@ import 'firebase/storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-  apiKey: 'AIzaSyAuv09ygP67re-9b3SKLyKyWmPur68ey5g',
-  authDomain: 'elicit-5039d.firebaseapp.com',
-  projectId: 'elicit-5039d',
-  storageBucket: 'elicit-5039d.appspot.com',
-  messagingSenderId: '698379837235',
-  appId: '1:698379837235:web:2e1bd830a420c094bc6846',
-  measurementId: 'G-KBWY7EP07S',
+  apiKey: '',
+  authDomain: 'elicit-9d.firebaseapp.com',
+  projectId: 'elicit-9d',
+  storageBucket: 'elicit-9d.appspot.com',
+  messagingSenderId: '6983795',
+  appId: '1:698379837235:web:2e1',
+  measurementId: 'G-KBWYS',
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
